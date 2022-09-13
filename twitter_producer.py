dcbc7d1ee6270d5d5a3b6eab5e11f0bbc86749a7
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-09-13 07:49:54 -0400

    twitter producer

---
+++ twitter_producer.py
@@ -0,0 +1,35 @@
+import tweepy
+import config
+from kafka import KafkaProducer
+
+client = tweepy.Client(config.BEARER_TOKEN,config.API_KEY,config.API_KEY_SECRET,config.ACCESS_TOKEN,config.ACCESS_TOKEN_SECRET)
+
+auth = tweepy.OAuth1UserHandler(config.API_KEY,config.API_KEY_SECRET,config.ACCESS_TOKEN,config.ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth);
+
+search_terms = ["Hadoop","Spark","Big Data","Hive"]
+
+class TwitterStream(tweepy.StreamingClient):
+    def on_connect(self):
+        print("Connected to Twitter Stream...")
+    
+    def on_data(self, data):
+        producer.send('test-topic',data)
+        print(data)
+        return True
+        
+    def on_error(self, status):
+        print("An Error has Occured " + status)
+
+
+stream = TwitterStream(config.BEARER_TOKEN)
+
+producer = KafkaProducer(bootstrap_servers=config.BOOTSTRAP_SERVER)
+
+for term in search_terms:
+    stream.add_rules(tweepy.StreamRule(term))
+    
+stream.filter(tweet_fields = ["referenced_tweets", "lang"])
+
+
